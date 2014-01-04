7b2ad0cdf5cd1e709dde3490dd18f157617c3c70
ainarsh@gmail.com <ainarsh@gmail.com>
2014-01-05 00:15:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-TXTGenerator
+# ![logo](http://snag.gy/PeDnh.jpg) TXTGenerator
 ============
 
-Generates n text files with same content.
+## Info
+
+Simple program made in Delphi XE5, what loads txt file on open. And Allows to generate the same file content on defined amount of txt files in seperate folder.
+
++ Button "Save" to change content of template.txt file directly from program;
++ Edit field with default value=200
++ Button "Generate", generates n text files with same content(from template.txt) in gen folder.
+
+In result, there will be generated files like 1.txt, 2.txt... etc.
---
+++ Unit1.pas
@@ -0,0 +1,65 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, IOUtils, Vcl.ComCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    SaveFile: TButton;
+    Generate: TButton;
+    Memo1: TMemo;
+    procedure LoadFile(Sender: TObject);
+    procedure SaveFileClick(Sender: TObject);
+    procedure GenerateClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.LoadFile(Sender: TObject);
+begin
+if FileExists('template.txt')then begin
+Memo1.Lines.LoadFromFile('template.txt');
+  end
+  else
+    ShowMessage('Failu template.txt neatrodu, iearaksti tekstu un izmanto pogu "SAVE" !!!');
+end;
+
+procedure TForm1.SaveFileClick(Sender: TObject);
+begin
+Memo1.Lines.SaveToFile('template.txt');
+end;
+
+procedure TForm1.GenerateClick(Sender: TObject);
+var
+  dirName, fName : String;
+  i, max: integer;
+begin
+  // Create a new directory
+  dirName := 'gen';
+
+  if NOT DirectoryExists(dirName) then
+    CreateDir(dirName);
+
+  fName := Edit1.Text;
+  max := StrToInt(fName);
+
+  for i := 1 to max do begin
+    fName := dirName + '\'+ IntToStr(i) + '.txt';
+    Memo1.Lines.SaveToFile( fName );
+  end;
+
+end;
+end.
